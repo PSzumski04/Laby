2cdfcf7c430bb09d3fc8f6e9c23e7cc26d18ec28
PSzumski04 <szumik.999999@gmail.com>
2025-10-26 14:41:16 +0100

    mała poprawka
    Fix logic for leap year calculation and output.

--- zad6.c
+++ zad6.c
@@ -9,6 +9,7 @@
     scanf("%d", &a);
 
      if (a  % 4 == 0 && (a % 100 != 0 || a % 400 == 0))
+              //musi byc podzielny przez 4 oraz niepodzielny przez 100 lub podzielny przez 400
      {
     printf("rok jest przestepny\n"), a;
      }
